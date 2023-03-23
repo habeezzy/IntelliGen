9c6b2fe2b1b86df2108b6d0c8b5dc881e6971764
Muhammad <mhabeebulah@gmail.com>
2023-03-23 18:40:28 -0400

    Mongodb connected

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -78,10 +78,10 @@ const CreatePost = () => {
   };
 
   return (
-    <section className="max-w-7xl mx-auto">
+    <section className="text-center max-w-7xl mx-auto">
       <div>
-        <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
-        <p className="mt-2 text-[#666e75] text-[14px] max-w-[500px]">Generate an imaginative image through DALL-E AI and share it with the community</p>
+        <h1 className="text-center font-extrabold text-[#222328] text-[32px]">Create</h1>
+        <p className="text-center mt-2 text-[#666e75] text-[14px] max-w-[500px]">Generate an imaginative image through DALL-E AI and share it with the community</p>
       </div>
 
       <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
